ac26a40fa22c58b1b3229258e711057938f5e5cb
Peter Emonds-Banfield <pebanfield@gmail.com>
2015-01-01 20:48:24 +0000

    removed favicon module to get past deploy env path issue

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -28,7 +28,7 @@ app.set('view engine', '.hbs');
 
 var rootPath = __dirname.replace('/src/server', '');
 
-app.use(favicon(rootPath + '/public/img/ibeam.ico'));
+//app.use(favicon(rootPath + '/public/img/ibeam.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
