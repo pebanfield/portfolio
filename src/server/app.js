6b9d7c8dd1c805e59e29fe701135da998b0424a0
Peter Emonds-Banfield <pebanfield@gmail.com>
2015-01-01 15:59:44 -0800

    mucking with paths

--- src/server/app.js
+++ src/server/app.js
@@ -68,8 +68,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(process.env.PORT, process.env.IP);
+//app.listen(process.env.PORT, process.env.IP);
 
 console.log("app started on port - " + process.env.PORT);
-//app.listen(3000);
+app.listen(3000);
 module.exports = app;
