57c1d9c5acd2d6f3cdefc1415d0dab1478563f6f
P.E. Banfield <pebanfield@gmail.com>
2016-10-04 14:03:20 -0700

    updated resume

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
-//var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -30,7 +29,6 @@ app.set('views', __dirname + '/views');
 
 var rootPath = __dirname.replace('/src/server/views', '');
 
-//app.use(favicon(rootPath + '/public/img/ibeam.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
